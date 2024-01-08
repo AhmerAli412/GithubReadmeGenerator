a13bf90b68c9a35386994943dcb6a772037b4e3e
Ahmer Ali <aliahmer289@gmail.com>
2024-01-08 14:35:30 +0500

    Hero img

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,11 +7,11 @@ const Footer = () => {
 <footer
   class="bg-neutral-200 text-center dark:bg-gray-900 border-t-2 lg:text-left">
   <div class="p-4 text-center text-neutral-700 dark:text-neutral-200">
-    Made with ❤️ by Ahmer Ali
+    Made with ❤️ by { }
     <a
       class="text-neutral-800 dark:text-neutral-400"
-      href="https://tw-elements.com/"
-      > </a>
+      href="https://github.com/AhmerAli412"
+      > Ahmer Ali </a>
   </div>
 </footer>
     </div>
